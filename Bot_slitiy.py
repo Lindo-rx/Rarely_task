e2ab596cbb41cce9cbc640ce1ac7bde51169a98d
Lindo-rx <lond1.aby@gmail.com>
2025-01-18 12:32:23 +0300

    Update Bot_slitiy.py

--- Bot_slitiy.py
+++ Bot_slitiy.py
@@ -3,11 +3,14 @@ from telebot import types
 bot = telebot.TeleBot('7959873800:AAH2pgOXXef0g6cm2fXLz54c38KZ-3TXb7E')
 @bot.message_handler(commands=['start'])
 def start_command(message):
+    a = f'@{message.from_user.username} здравствуйте!'
     markup = types.ReplyKeyboardMarkup()
     btn1 = types.KeyboardButton('Зарегистрироваться', )
     btn2 = types.KeyboardButton('Войти')
     markup.row(btn1, btn2)
+    bot.send_message(message.chat.id, a)
     bot.send_message(message.chat.id, 'Вас приветствует ITeam помощник!', reply_markup=markup)
+
 @bot.message_handler(content_types=['text'])
 def func(message):
 
@@ -15,39 +18,22 @@ def func(message):
 
         bot.send_message(message.chat.id, 'Сейчас тебя зарегистрируем! Введи своё имя:')
         bot.register_next_step_handler(message, user_name)
-
-def user_name(message):
-    global name
-    name = message.text.strip()
-    bot.send_message(message.chat.id, 'Введи свой пароль:')
-    bot.register_next_step_handler(message, user_pass)
-def user_pass(message):
-    password = message.text.strip()
-    bot.send_message(message.chat.id, 'Пользователь зарегистрирован')
-    bot.register_next_step_handler(message, answer)
-def answer(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton("Посмотреть своё расписание")
-    btn2 = types.KeyboardButton("Записаться на курс")
-    btn3 = types.KeyboardButton("Информация по курсам")
-    markup.row(btn1)
-    markup.row(btn2, btn3)
-    bot.send_message(message.chat.id, text="Что хочешь сделать?", reply_markup=markup)
-elif (message.text == 'Посмотреть своё расписание'):
+    elif message.text == 'Посмотреть своё расписание':
         bot.send_message(message.chat.id, text="Всё ок")
     elif (message.text == 'Записаться на курс'):
-            bot.send_message(message.chat.id, text="Всё ок")
+        bot.send_message(message.chat.id, text="Всё ок")
     elif (message.text == 'Информация по курсам'):
-            bot.send_message(message.chat.id, text="Всё ок")
+        bot.send_message(message.chat.id, text="Всё ок")
 
     elif (message.text == 'Войти'):
-        markup = types.ReplyKeyboardMarkup()
-        btn1 = types.KeyboardButton("Посмотреть своё расписание")
-        btn2 = types.KeyboardButton("Записаться на курс")
-        btn3 = types.KeyboardButton("Информация по курсам")
-        markup.row(btn1)
-        markup.row(btn2, btn3)
-        bot.send_message(message.chat.id, text="Что хочешь сделать?", reply_markup=markup)
+        bot.send_message(message.chat.id, 'Сейчас тебя 100100100111001 в нашу программу! Введи своё имя:')
+        bot.register_next_step_handler(message, user_name)
+    elif message.text == 'Посмотреть своё расписание':
+        bot.send_message(message.chat.id, text="Всё ок")
+    elif (message.text == 'Записаться на курс'):
+        bot.send_message(message.chat.id, text="Всё ок")
+    elif (message.text == 'Информация по курсам'):
+        bot.send_message(message.chat.id, text="Всё ок")
     elif (message.text == 'Посмотреть своё расписание'):
         bot.send_message(message.chat.id, text="Всё ок")
 
@@ -56,4 +42,22 @@ elif (message.text == 'Посмотреть своё расписание'):
 
     elif (message.text == 'Информация по курсам'):
         bot.send_message(message.chat.id, text="Всё ок")
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введи свой пароль:')
+    bot.register_next_step_handler(message, user_pass)
+def user_pass(message):
+    password = message.text.strip()
+    bot.send_message(message.chat.id, 'Пользователь зарегистрирован (или успешно зашёл в свой аккаунт)')
+    bot.register_next_step_handler(message, answer)
+    answer(message)
+def answer(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("Посмотреть своё расписание")
+    btn2 = types.KeyboardButton("Записаться на курс")
+    btn3 = types.KeyboardButton("Информация по курсам")
+    markup.row(btn1)
+    markup.row(btn2, btn3)
+    bot.send_message(message.chat.id, text="Что хочешь сделать?", reply_markup=markup)
 bot.polling(none_stop=True)
